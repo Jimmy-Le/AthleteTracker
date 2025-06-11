8e3d29de63803ef9ebc88b3fb04f361790a9a778
Jimmy <jimmy_le_@hotmail.com>
2025-06-10 22:42:39 -0400

    created project

---
+++ Athlete.py
@@ -0,0 +1,8 @@
+class Athlete:
+    def __init__(self, name, age, country, salary):
+        self.name = name
+        self.age = age
+        self.country = country
+        self.salary = salary
+
+
